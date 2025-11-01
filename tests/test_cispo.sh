9e942e1f55363f6b2b574fb4e73b43ef1b9571b3
jd.kim <kekmodel@gmail.com>
2025-11-01 18:40:50 +0900

    Increase rollout max response length to 8192 in test_cispo.sh

--- tests/test_cispo.sh
+++ tests/test_cispo.sh
@@ -55,7 +55,7 @@ ROLLOUT_ARGS=(
    --num-rollout 100
    --rollout-batch-size 8
    --n-samples-per-prompt 4
-   --rollout-max-response-len 1024
+   --rollout-max-response-len 8192
    --rollout-temperature 1.0
 
    --global-batch-size 32
